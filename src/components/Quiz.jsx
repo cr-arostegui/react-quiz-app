51456e0c72189c0f183f09956e5a9844c1311211
Cristhian Arostegui <cr.arostegui@gmail.com>
2024-01-31 16:10:17 -0400

    Add Quiz component to render questions and save user answered questions

---
+++ src/components/Quiz.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+
+import QUESTIONS from '../questions';
+
+export default function Quiz() {
+  const [userAnswer, setUserAnswer] = useState([]);
+
+  const activeQuestionIndex = userAnswer.length;
+
+  function handleSelectAnswer(selectedAnswer) {
+    setUserAnswer((prevUserAnswers) => {
+      return [...prevUserAnswers, selectedAnswer];
+    });
+  }
+
+  return (
+    <div>
+      <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+      <ul id="answers">
+        {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+          <li key={answer} className="answer">
+            <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
