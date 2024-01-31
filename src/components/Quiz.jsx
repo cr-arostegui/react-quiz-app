86ef86a64f7c5af63854c66b926438247a9f610e
Cristhian Arostegui <cr.arostegui@gmail.com>
2024-01-31 16:26:56 -0400

    Suffle questions and add quiz logic to control when quiz is complete, show a complete UI when quiz is finished

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
 
 import QUESTIONS from '../questions';
+import quizCompleteImg from '../assets/quiz-complete.png';
 
 export default function Quiz() {
   const [userAnswer, setUserAnswer] = useState([]);
 
   const activeQuestionIndex = userAnswer.length;
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
   function handleSelectAnswer(selectedAnswer) {
     setUserAnswer((prevUserAnswers) => {
@@ -13,16 +15,32 @@ export default function Quiz() {
     });
   }
 
+  if (quizIsComplete) {
+    return (
+      <div id="summary">
+        <img src={quizCompleteImg} alt="Trophy Icon" />
+        <h2>Quiz Completed!</h2>
+      </div>
+    );
+  }
+
+  const suffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  suffledAnswers.sort(() => Math.random() - 0.5);
+
   return (
-    <div>
-      <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
-      <ul id="answers">
-        {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
-          <li key={answer} className="answer">
-            <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
-          </li>
-        ))}
-      </ul>
+    <div id="quiz">
+      <div id="question">
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+          {suffledAnswers.map((answer) => (
+            <li key={answer} className="answer">
+              <button onClick={() => handleSelectAnswer(answer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
